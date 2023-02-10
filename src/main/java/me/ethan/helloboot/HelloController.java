968e5b6eee620659246ee9685405dffa6207104e
EthanKim <hey0507@gmail.com>
2023-02-11 00:01:00 +0900

    [Feat] HelloController 생성 - GetMapping Hello 를 받는 컨트롤러 생성 - HTTP 요청과 응답에 대해 확인 - HTTPie CLI, App 기능 확인

---
+++ src/main/java/me/ethan/helloboot/HelloController.java
@@ -0,0 +1,15 @@
+package me.ethan.helloboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // HTML 을 통채로 리턴하는 것이 아닌, HTTP 요청에 대한 응답을 특정 타입으로 리턴하는 컨트롤러
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(String name){
+        return "hello " + name;
+    }
+
+
+}
