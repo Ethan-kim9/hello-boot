18b68b6a3370aee954db8e5455fc8c5427014106
EthanKim <hey0507@gmail.com>
2023-02-13 13:17:01 +0900

    [Feat] Dependency Injection 관련 - DI 에 대한 설명과 Controller, Service 의 스프링 컨테이너를 통한 주입방법

--- src/main/java/me/ethan/helloboot/HellobootApplication.java
+++ src/main/java/me/ethan/helloboot/HellobootApplication.java
@@ -6,7 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,29 +18,31 @@ public class HellobootApplication {
     public static void main(String[] args) {
         System.out.println("Hello Containerless standalone Application");
 
+        // 스프링 컨테이너 (ApplicationContext 생성)
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh();
+
+
         TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
         WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
-            HelloController helloController = new HelloController();
-
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
+            servletContext.addServlet("hello", new HttpServlet() {
                         @Override
                         protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
                             // 인증, 보안 , 다국어 처리 , 공통 기능
-
                             if(req.getRequestURI().equals("/hello")
                                     && req.getMethod().equals(HttpMethod.GET.name())
                             ){
-
                                 String name = req.getParameter("name");
+
+                                // Bean 을 ApplicationContext 로 가져오는 것
+                                HelloController helloController = applicationContext.getBean(HelloController.class);
                                 String hello = helloController.hello(name);
 
-                                resp.setStatus(HttpStatus.OK.value());
-                                resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                                resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                                 resp.getWriter().println(hello);
                             }
-                            else if(req.getRequestURI().equals("/user")){
-                                resp.getWriter().println("foo");
-                            }else{
+                            else{
                                 resp.setStatus(HttpStatus.NOT_FOUND.value());
                             }
                         }
