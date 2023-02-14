5a81de178e577008591277c7c08d0d56994ac23d
EthanKim <hey0507@gmail.com>
2023-02-14 22:58:20 +0900

    Refactor : applicationContext.refresh() 를 통해 스프링 컨테이너가 초기화되는 부분을, onRefresh 메서드를 익명클래스로 서블릿 컨테이너(dispatcherServlet) 까지 한번에 초기화함

--- src/main/java/me/ethan/helloboot/HellobootApplication.java
+++ src/main/java/me/ethan/helloboot/HellobootApplication.java
@@ -9,33 +9,28 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello Containerless standalone Application");
 
-        // 스프링 컨테이너 (ApplicationContext 생성)
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-        applicationContext.registerBean(HelloController.class);
-        /**
-         * HelloController 를 생성하는 과정에서, 주입하는 Service 등의 빈을 확인하고 그 과정에서 구현체의 Bean 도 스프링 컨테이너에서 관리하게 됨
-         * - 흐름상, SimpleHelloService 가 먼저 Bean 등록이 되어야할 것 같지만, 등록되는 순서는 상관없음 -> 스프링에서 알아서 관리
+        /* *
+         * 기존의 스프링 컨테이너 초기화, 스프링 컨테이너를 이용해 서블릿 컨테이너를 사용하는 두가지 절차를,
+         * 스프링 컨테이너 초기화와 함께 서블릿 컨테이너도 모두 초기화 되게 통합
          */
-        applicationContext.registerBean(SimpleHelloService.class);// 구현체를 Bean 으로 등록함
 
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this))
+                            .addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
-
-
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet",
-                    new DispatcherServlet(applicationContext)
-                    ).addMapping("/*");
-        });
-        /**
-         * 기존 Servlet 을 하나하나 등록하던 것을 DispatcherServlet 으로 변경해줌
-         * Servlet에 url 하나하나 연동해주던 것을 Controller 에서 받게끔 변경해줌
-         * */
-
-
-        webServer.start();
     }
+
 }
