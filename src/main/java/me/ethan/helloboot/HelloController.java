72f02cdd680fcd451a074cdbd580320057bf7073
EthanKim <hey0507@gmail.com>
2023-02-14 22:40:24 +0900

    Refactor : DispatcherServlet 으로 기능 변경 - 기존 Servlet 을 직접 하드코딩하여 생성해주던 코드를 변경하여, DispatcherServlet 을 통한 URL 매핑 등 빈 관리가 이루어질 수 있게 코드 수정 - 메서드, 클래스 레벨에서 URL 추가

--- src/main/java/me/ethan/helloboot/HelloController.java
+++ src/main/java/me/ethan/helloboot/HelloController.java
@@ -1,8 +1,15 @@
 package me.ethan.helloboot;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+//@Controller
+@RequestMapping // Dispatcher Servlet 에서 메서드레벨의 매핑값을 찾을 수 있게, 클래스레벨에서 찾게해줌
+//
 public class HelloController {
     private final HelloService helloService;// 초기화 진행
 
@@ -11,7 +18,11 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping("/hello") //dispatcher Servlet 에서, applicationContext(servlet) 를 파라미터로 생성될 때, 모든 bean 을 순회하며 mapping 정보를 색인
+    @ResponseBody
     public String hello(String name){
         return helloService.sayHello(Objects.requireNonNull(name));
     }
+
+    // String 으로 Return 할 경우, view 를 찾음
 }
--- src/main/java/me/ethan/helloboot/HellobootApplication.java
+++ src/main/java/me/ethan/helloboot/HellobootApplication.java
@@ -1,17 +1,10 @@
 package me.ethan.helloboot;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import java.io.IOException;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication {
 
@@ -19,7 +12,7 @@ public class HellobootApplication {
         System.out.println("Hello Containerless standalone Application");
 
         // 스프링 컨테이너 (ApplicationContext 생성)
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HelloController.class);
         /**
          * HelloController 를 생성하는 과정에서, 주입하는 Service 등의 빈을 확인하고 그 과정에서 구현체의 Bean 도 스프링 컨테이너에서 관리하게 됨
@@ -30,31 +23,19 @@ public class HellobootApplication {
         applicationContext.refresh();
 
 
-        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
-                        @Override
-                        protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                            // 인증, 보안 , 다국어 처리 , 공통 기능
-                            if(req.getRequestURI().equals("/hello")
-                                    && req.getMethod().equals(HttpMethod.GET.name())
-                            ){
-                                String name = req.getParameter("name");
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
-                                // Bean 을 ApplicationContext 로 가져오는 것
-                                HelloController helloController = applicationContext.getBean(HelloController.class);
-                                String hello = helloController.hello(name);
-
-                                resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                                resp.getWriter().println(hello);
-                            }
-                            else{
-                                resp.setStatus(HttpStatus.NOT_FOUND.value());
-                            }
-                        }
-                    }).addMapping("/*");
-
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("dispatcherServlet",
+                    new DispatcherServlet(applicationContext)
+                    ).addMapping("/*");
         });
+        /**
+         * 기존 Servlet 을 하나하나 등록하던 것을 DispatcherServlet 으로 변경해줌
+         * Servlet에 url 하나하나 연동해주던 것을 Controller 에서 받게끔 변경해줌
+         * */
+
+
         webServer.start();
     }
 }
