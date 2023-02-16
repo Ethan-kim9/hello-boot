3bfba0a97713d9f357c29815cea9d293fe1b0f54
EthanKim <hey0507@gmail.com>
2023-02-16 22:19:48 +0900

    Test : API 정상 기능 확인을 위한 개발자 테스트용 코드 작성

---
+++ src/test/java/me/ethan/helloboot/HelloApiTest.java
@@ -0,0 +1,31 @@
+package me.ethan.helloboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloApiTest {
+    @Test
+    void helloApi(){
+        // http:localhost:8080/hello?name=Spring
+
+
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> response =
+                rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+
+        //status code 200
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        //header (Content-Type) text/plain
+        assertThat(response.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        // body Hello Spring
+        assertThat(response.getBody()).isEqualTo("Hello Spring");
+    }
+}
