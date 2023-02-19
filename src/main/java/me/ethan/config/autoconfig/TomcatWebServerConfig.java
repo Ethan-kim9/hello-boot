1d0d17f8a0398e840f310794ec9594dee83b65ff
EthanKim <hey0507@gmail.com>
2023-02-19 23:59:13 +0900

    [Update] Tomcat -> Jetty - Conditional 어노테이션 - Condition 클래스 구현 및 matches 메서드를 통한 주입 클래스 식별

--- src/main/java/me/ethan/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/me/ethan/config/autoconfig/TomcatWebServerConfig.java
@@ -4,8 +4,13 @@ import me.ethan.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfig.TomCatCondition.class)
 public class TomcatWebServerConfig {
 
     @Bean("tomcatWebServerFactory")
@@ -13,4 +18,11 @@ public class TomcatWebServerConfig {
         return new TomcatServletWebServerFactory();
     }
 
+    static class TomCatCondition implements Condition {
+
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
+    }
 }
