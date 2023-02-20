7abc76452628df111977b216cee0907fa26ae0fb
EthanKim <hey0507@gmail.com>
2023-02-20 11:22:52 +0900

    [Test] ConditionalMyOnClass 생성 - 해당 클래스가 존재할 때만 Condition 을 true 로 리턴해주고 아닐 경우엔 false 로 Context 를 제어

--- src/main/java/me/ethan/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/me/ethan/config/autoconfig/JettyWebServerConfig.java
@@ -1,27 +1,16 @@
 package me.ethan.config.autoconfig;
 
+import me.ethan.config.ConditionalMyOnClass;
 import me.ethan.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
-@Conditional(JettyWebServerConfig.JettyCondition.class) // Spring Container 를 통한 주입
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
-
     @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory(){
         return new JettyServletWebServerFactory();
-    }
-
-    static class JettyCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return true;
-        }
     }
 }
