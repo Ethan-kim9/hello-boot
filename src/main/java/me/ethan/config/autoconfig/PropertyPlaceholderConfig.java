bf5958cdc46c75c0a284f26d0abaa1cd6600a5c6
EthanKim <hey0507@gmail.com>
2023-02-20 19:48:53 +0900

    [Update] PropertyConfig 설정 방법 변경 - 기존 Environment 를 주입하여 구현하는 방식에서 @Value 라는 어노테이션을 이용하게 변경 -> 이경우, PropertyPlaceholderConfig 의 클래스처럼, postProcessBeanFactory 를 구현해주는 메서드가 필요함

---
+++ src/main/java/me/ethan/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,14 @@
+package me.ethan.config.autoconfig;
+
+import me.ethan.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+
+    @Bean
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
