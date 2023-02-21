9e71642b2356732a8ad753d71543b7e9470fcc24
EthanKim <hey0507@gmail.com>
2023-02-21 13:02:13 +0900

    Feat : JdbcTemplate, JdbcTransactionManager 생성 - @Transaction 테스트 진행

--- src/test/java/me/ethan/helloboot/DataSourceTest.java
+++ src/test/java/me/ethan/helloboot/DataSourceTest.java
@@ -1,20 +1,13 @@
 package me.ethan.helloboot;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = HellobootApplication.class)
-@TestPropertySource("classpath:/application.properties")
+@HellobootDatabaseTest
 public class DataSourceTest {
 
     @Autowired DataSource dataSource;
---
+++ src/test/java/me/ethan/helloboot/JdbcTemplateTest.java
@@ -0,0 +1,41 @@
+package me.ethan.helloboot;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@HellobootDatabaseTest
+public class JdbcTemplateTest {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void init(){
+        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+    }
+
+    @Test
+    void insertAndQuery(){
+        jdbcTemplate.update("insert into hello values(?,?)", "Ethan",2);
+        jdbcTemplate.update("insert into hello values(?,?)", "Spring",1);
+
+        Long count = jdbcTemplate.queryForObject("select count (*) from hello", Long.class);
+        assertThat(count).isEqualTo(2);
+    }
+    @Test
+    void transactionTest(){
+        jdbcTemplate.update("insert into hello values(?,?)", "Ethan",2);
+        jdbcTemplate.update("insert into hello values(?,?)", "Spring",1);
+
+        Long count = jdbcTemplate.queryForObject("select count (*) from hello", Long.class);
+        assertThat(count).isEqualTo(2);
+    }
+
+
+}
